667da93fc225b45dbcfaf02dd7eb0f66bbe0a1f3
Wild fate <cherkasovavika@mail.ru>
2017-10-17 22:37:52 +0300

    Добавлено чтение файла, комментарии и файл readme.

--- Assignment_problem/renderer.js
+++ Assignment_problem/renderer.js
@@ -1,61 +1,100 @@
 /**
- * Created by Виктория on 03.08.2017.
+ * Created by Виктория on 14.10.2017.
  */
 const electron = require('electron')
-const ipc = electron.ipcRenderer
 const $ = selector => document.querySelector(selector)
 const marked = require('marked')
+const fs = require('fs')
+const path = require('path')
 
-const $htmlView = $('.rendered-html')
 const $matrSize = $('#matrSize')
 const $inptBtn = $('#inptBtn')
 const $matr = $('#matr')
 const $calcBtn = $('#calcBtn')
 const $removeBtn = $('#removeBtn')
 const $table = $('#table')
+const $fromFileBtn = $('#fromFileBtn')
+const $resultLabel = $('#resultLabel')
 
+//файл с матрицей
+const dataFile = path.resolve('./data.txt')
+
+//размер матрицы
 let matrSize
+//значения матрицы
 let matrVals
 
 let xy, yx             // Паросочетания: xy[разраб], yx[задача]
 let vx, vy            // Альтернирующее дерево vx[разраб], vy[задача]
 let minrow, mincol     // Способности, изученность
 
+//кнопка "Загрузить данные из файла"
+$fromFileBtn.addEventListener('click', () => {
+    //задать размер матрицы
+    matrSize = parseInt($matrSize.value)
 
-ipc.on('file-opened', (event, file, content) => {
+    //инициализация полей ввода для матрицы
+    $inptBtn.click()
+
+//чтение из файла
+fs.readFile(dataFile, 'utf8', function(err, data) {
+    if (err) throw err;
+    let numerics = data.split(' ')
+    let row = 0
+    let col = 0
+    for (let i = 0; i < numerics.length; i++) {
+        if(i >= matrSize && (i%matrSize) == 0){
+            //console.log('i = ' + i)
+            row++
+            col = 0
+        }
+        //console.log(row + '_' + col)
+        //вывод значений на форму
+        let inpt = document.getElementById(row + '_' + col)
+        inpt.value = numerics[i]
+        col++
+    }
+    //console.log('OK: ' + dataFile)
+    //console.log(data)
+})
 
 })
 
+//кнопка очистки
 $removeBtn.addEventListener('click', () => {
+    //проход по всем элемента таблицы
     while($matr.children.length>0)
-{
-    for (let elNum = 0; elNum < $matr.children.length; elNum++) {
-       // console.log($matr.children[elNum])
-        $matr.children[elNum].remove()
+    {
+
+        for (let elNum = 0; elNum < $matr.children.length; elNum++) {
+           // console.log($matr.children[elNum])
+            $matr.children[elNum].remove()
+        }
     }
-}
+    //очистка поля вывода ответа
+    $resultLabel.innerText = ''
 })
 
+//кнопка "Ввести значений"
 $inptBtn.addEventListener('click', () => {
-    while($matr.children.length>0)
-{
-    for (let elNum = 0; elNum < $matr.children.length; elNum++) {
-        // console.log($matr.children[elNum])
-        $matr.children[elNum].remove()
-    }
-}
+    //очистка
+    $removeBtn.click()
+    //ввод размера матрицы
     matrSize = parseInt($matrSize.value)
 //    console.log("Matr size = " + matrSize)
+    //создание массива значений
     matrVals = new Array(matrSize)
     for (var i = 0; i < matrSize; i++) {
         matrVals[i] = new Array(matrSize)
     }
+    //массивы для промежуточных вычислений
     xy = new Array(matrSize)
     yx = new Array(matrSize)
     vx = new Array(matrSize)
     vy = new Array(matrSize)
     minrow = new Array(matrSize)
     mincol = new Array(matrSize)
+    //отрисовка ячеек для ввода значений матрицы
     for(let row = 0; row<matrSize; row++){
         let col1 = document.createElement('tr')
         for(let col=0; col<matrSize; col++){
@@ -70,10 +109,13 @@ $inptBtn.addEventListener('click', () => {
     }
 })
 
+//кнопка "Решить"
 $calcBtn.addEventListener('click', () => {
+    //получение таблицы из формы
     var trs = $table.getElementsByTagName('tr')
     var tds = null
 
+    //проход по полям таблицы
     for (var i=0; i<trs.length; i++)
     {
         tds = trs[i].getElementsByTagName('td')
@@ -81,76 +123,92 @@ $calcBtn.addEventListener('click', () => {
         {
             let inpt = tds[n].getElementsByTagName('input')
             let nums = inpt[0].id.split('_')
+            //присвоение значений матрице
             matrVals[parseInt(nums[1])][parseInt(nums[0])] = parseInt(inpt[0].value)
 //            console.log('Value = ' + matrVals[parseInt(nums[0])][parseInt(nums[1])])
         }
     }
 
+    //присвоение максимального значений
     for (let i=0; i<matrSize; i++)
     {
         minrow[i] = Number.POSITIVE_INFINITY
         mincol[i] = Number.POSITIVE_INFINITY
     }
+    //поиск минимума в каждой строке
     for (let i=0; i<matrSize; i++) {
         for (let j = 0; j < matrSize; j++) {
             minrow[i] = Math.min(minrow[i], matrVals[i][j])
         }
         //console.log('minCol ' + i + ' = ' + minrow[i])
     }
+    //поиск минимума в каждом столбце
     for (let j = 0; j < matrSize; j++) {
         for (let i = 0; i < matrSize; i++) {
             mincol[j] = Math.min(mincol[j], matrVals[i][j] - minrow[i])
         }
         //console.log('minRow ' + j + ' = ' + mincol[j])
     }
-for (let i=0; i<matrSize; i++)
-{
-    xy[i] = -1
-    yx[i] = -1
-}
-
-for (let c=0; c<matrSize; ) {
+    //присвоение первоначальных значений
     for (let i=0; i<matrSize; i++)
     {
-        vx[i] = 0
-        vy[i] = 0
+        xy[i] = -1
+        yx[i] = -1
     }
-    let k = 0
-    for (let i=0; i<matrSize; ++i) {
-        if (xy[i] == -1 && dotry(i)) {
-            ++k
+
+    //вычисление, пока количество нулей не будет равно размеру матрицы
+    for (let c=0; c<matrSize; ) {
+        //присвоение первоначальных значений
+        for (let i=0; i<matrSize; i++)
+        {
+            vx[i] = 0
+            vy[i] = 0
         }
-    }
-    c += k
-    if (k == 0) {
-        let z = Number.POSITIVE_INFINITY
+        let k = 0
         for (let i=0; i<matrSize; ++i) {
-            if (vx[i]) {
-                for (let j = 0; j < matrSize; ++j)
-                    if (!vy[j]) {
-                        z = Math.min(z, matrVals[i][j] - minrow[i] - mincol[j])
-                    }
+            if (xy[i] == -1 && dotry(i)) {
+                ++k
             }
         }
-        for (let i=0; i<matrSize; ++i) {
-            if (vx[i]) minrow[i] += z
-            if (vy[i]) mincol[i] -= z
+        c += k
+        if (k == 0) {
+            let z = Number.POSITIVE_INFINITY
+            for (let i=0; i<matrSize; ++i) {
+                if (vx[i]) {
+                    for (let j = 0; j < matrSize; ++j)
+                        if (!vy[j]) {
+                            z = Math.min(z, matrVals[i][j] - minrow[i] - mincol[j])
+                        }
+                }
+            }
+            //необхоодимые вычитания и прибавления
+            for (let i=0; i<matrSize; ++i) {
+                if (vx[i]) minrow[i] += z
+                if (vy[i]) mincol[i] -= z
+            }
         }
     }
-}
 
-let ans = 0
-for (let i=0; i<matrSize; ++i) {
-    ans += matrVals[i][xy[i]]
-}
-console.log('Res = ' + ans)
-//printf ("%d\n", ans);
-for (let i=0; i<matrSize; ++i) {
-    console.log('Nums = ' + (xy[i] + 1))
-  //  printf("%d ", xy[i] + 1);
-}
+    //формирование результата
+    let result = 'Результат: '
+    let ans = 0
+    for (let i=0; i<matrSize; ++i) {
+        result += matrVals[i][xy[i]] + '+'
+        ans += matrVals[i][xy[i]]
+        //выделение яцейки красным цветом
+        let inpt = document.getElementById(xy[i] + '_' + i)
+        inpt.style = 'background-color: #d9534f'
+    }
+    if(result.length > 0) {
+        result = result.substring(0, result.length - 1) + '=' + ans
+    }
+    //console.log('Res = ' + ans)
+
+    //вывод результата
+    $resultLabel.innerText = result
 })
 
+//вычитание минимума столбца и строки
 function dotry (i) {
     if (vx[i]) return false;
     vx[i] = true;
